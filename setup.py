aa16135a04059c3ad99f6bf9fa59f5806dfdbb00
k24mr <@kfzteile24.de>
2020-09-29 11:22:32 +0200

    changed it back to 7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='facebook-ads-performance-downloader',
-    version='5.0.1',
+    version='4.1.1',
 
     description=("Downloads data from the Facebook Ads API to local files"),
 
